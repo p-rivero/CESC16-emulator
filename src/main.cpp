4b07f0da3be7edbdf73b61638c35abd93ad90f43
privero <polriverosallent@gmail.com>
2021-06-02 21:15:30 +0200

    Added clock emulation

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,15 @@
 #include <thread>
 #include <functional>
 
+const double FREQ = 1600;    // 2 MHz 2000000
 
 CPU *cpu;
+
+void fatal_error(const char* msg) {
+    delete cpu;
+    fprintf(stderr, "%s\n", msg);
+    exit(EXIT_FAILURE);
+}
 
 void read_bin(std::ifstream& hex_file) {
     uint32_t address = 0;
@@ -15,20 +22,12 @@ void read_bin(std::ifstream& hex_file) {
         assert(hex_file >> std::hex >> low);
         cpu->write_ROM(address++, high, low);
 
-        if (address > 0xFFFF) {
-            // File too large for 16 bits of address space
-            delete cpu;
-            printf("Error: ROM file is too large\n");
-            exit(EXIT_FAILURE);
-        }
+        // File too large for 16 bits of address space
+        if (address > 0xFFFF) fatal_error("Error: ROM file is too large");
     }
 
     // Make sure there is no leftover input
-    if (not hex_file.eof()) {
-        delete cpu;
-        printf("Error: make sure the ROM file is a valid binary file\n");
-        exit(EXIT_FAILURE);
-    }
+    if (not hex_file.eof()) fatal_error("Error: make sure the ROM file is a valid binary file");
 }
 
 void timer_start(std::function<void(void)> func, unsigned int interval) {
@@ -57,26 +56,36 @@ int main(int argc, char **argv) {
 
     // Enter program in ROM
     std::ifstream hex_file(argv[1], std::fstream::in);
-    if (not hex_file) {
-        delete cpu;
-        perror("ROM file could not be opened");
-        exit(EXIT_FAILURE);
-    }
+    if (not hex_file) fatal_error("ROM file could not be opened");
     read_bin(hex_file);
     hex_file.close();
     
-    // Schedule timer for calling update() every 16 milliseconds
-    timer_start(call_update, 16);
+    // Schedule timer for calling update() every 30 milliseconds
+    timer_start(call_update, 30);
 
     // Give some time for the ncurses window to initialize.
     // Otherwise, if the program sends outputs too soon, the first chars wouldn't be displayed
     std::this_thread::sleep_for(std::chrono::milliseconds(20));
 
+
+    const int sleep_ms = 10;
+    // Amount of cycles to execute every (sleep_ms) millisecods
+    int CYCLES = FREQ * double(sleep_ms) / 1000.0;
+
+    if (CYCLES<16) fatal_error("Target clock frequency too low"); // Todo: if CYCLES<16, increase sleep_ms and try again
+
     // Execute program
+    int32_t extra_cycles = 0;
     while (true) {
-        cpu->execute(50);
+        auto x = std::chrono::steady_clock::now() + std::chrono::milliseconds(10);
+        // Store the used extra cycles and subtract them from the next execution
+        extra_cycles = cpu->execute(CYCLES - extra_cycles);
+
+        if (std::chrono::steady_clock::now() > x)
+            fatal_error("Target clock frequency too high for real-time emulation, try a slower clock");
+        
+        std::this_thread::sleep_until(x);
     }
 
-    delete cpu;
-    exit(EXIT_SUCCESS);
+    fatal_error("UNREACHABLE");
 }
